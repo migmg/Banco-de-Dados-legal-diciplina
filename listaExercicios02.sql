d4252f3f8a59978a56395307a23f2efa0023bf1c
migmg <102702140+migmg@users.noreply.github.com>
2023-09-26 20:29:01 -0300

    Update listaExercicios02.sql

--- listaExercicios02.sql
+++ listaExercicios02.sql
@@ -4,3 +4,13 @@ BEGIN
     SELECT AutorID, Nome
     FROM Autores;
 END;
+
+CREATE PROCEDURE sp_LivrosPorCategoria
+    @NomeCategoria NVARCHAR(100)
+AS
+BEGIN
+    SELECT Livros.Titulo
+    FROM Livros
+    INNER JOIN Categorias ON Livros.CategoriaID = Categorias.CategoriaID
+    WHERE Categorias.Nome = @NomeCategoria;
+END;
